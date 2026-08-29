9d7492cc20e50a090f85521a5fb52507dd0883c1
cliff <a0979326987@gmail.com>
2026-08-30 04:30:47 +0800

    feat(ui):add a putput label

--- include/ui/MainWindow.hpp
+++ include/ui/MainWindow.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <QWidget>
 #include <QPushButton>
+#include <QLabel>
 
 class AppController; // 前置宣告，防止互相依賴
 
@@ -9,9 +10,15 @@ class MainWindow : public QWidget {
 
 public:
     MainWindow(QWidget* parent = nullptr);
-
+    void showMessage(const QString& text); 
+private slots:
+    void onFirst(); // 預設
+    void onSecond();
+    void onThird();
 private :
+
     QPushButton* f_button;
     QPushButton* s_button;
     QPushButton* thi_button;
+    QLabel* outputLabel;
 };
--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -11,6 +11,9 @@ MainWindow::MainWindow(QWidget* parent)
 	resize(400, 300);
 
 	//建立物件
+	outputLabel = new QLabel("尚未執行任何動作", this);
+	outputLabel->setWordWrap(true);
+
 	f_button = new QPushButton("first_button", this);
 	s_button = new QPushButton("secound_button", this);
 	thi_button = new QPushButton("third_button", this);
@@ -20,4 +23,26 @@ MainWindow::MainWindow(QWidget* parent)
 	layout->addWidget(f_button);
 	layout->addWidget(s_button);
 	layout->addWidget(thi_button);
+
+	//接線
+	connect(f_button, &QPushButton::clicked, this, &MainWindow::onFirst);
+
+}
+
+void MainWindow::showMessage(const QString& text) {
+	outputLabel->setText(text);
+}
+//觸發函式(測試用)
+void MainWindow::onFirst() {
+	this->showMessage("按下測試按鈕");
+}
+
+
+void MainWindow::onSecond() {
+	// 暫時空著
+
+}
+
+void MainWindow::onThird() {
+	// 暫時空著
 }
