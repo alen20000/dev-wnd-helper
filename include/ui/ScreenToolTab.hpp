9c249a2faf574f035a0d1cb17fbd53e9975ebc0e
cliff <a0979326987@gmail.com>
2026-09-15 19:18:33 +0800

    chore(ScreenToolTab):clean up header declarations and setting includes

--- include/ui/ScreenToolTab.hpp
+++ include/ui/ScreenToolTab.hpp
@@ -1,10 +1,24 @@
 #pragma once
 
 #include <QWidget>
-
+#include <QPushButton>
 class ScreenToolTab : public QWidget {
-    Q_OBJECT
+	Q_OBJECT
+
 public:
     explicit ScreenToolTab(QWidget* parent = nullptr);
     ~ScreenToolTab() = default;
+private slots:
+	/* * @brief 「放大鏡」按鈕點擊事件，切換放大鏡功能
+	 */
+	void toggleMagnifier();
+
+private:
+
+	//Layout
+	QVBoxLayout* layout;
+
+	//Btn
+	QPushButton* m_maginifer;
+
 };
--- src/ui/ScreenToolTab.cpp
+++ src/ui/ScreenToolTab.cpp
@@ -1,9 +1,19 @@
 #include "ui/ScreenToolTab.hpp"
 #include <QVBoxLayout>
-
+#include <QPushButton>
 ScreenToolTab::ScreenToolTab(QWidget* parent)
     : QWidget(parent)
 {
+
+
+    // Layout
     QVBoxLayout* layout = new QVBoxLayout(this);
-    // 後續可以在這裡加入你的分頁 UI 元件
+
+    // Btn
+    QPushButton* m_maginifer = new QPushButton("Magnifier", this);
+    layout->addWidget(m_maginifer);
+}
+
+void ScreenToolTab::toggleMagnifier() {
+    // 預設:放大鏡切換
 }
